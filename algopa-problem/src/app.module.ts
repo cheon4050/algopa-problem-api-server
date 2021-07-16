df0949d484de3b8dd09a0dbfcc3c4068f2224d36
whddk4415 <whddk4415@gmail.com>
2021-07-16 12:07:01 +0900

    build: import neo4j module to app.module.ts

--- algopa-problem/src/app.module.ts
+++ algopa-problem/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProblemsModule } from './problems/problems.module';
 import * as Joi from '@hapi/joi';
+import { Neo4jConfig, Neo4jModule } from 'nest-neo4j';
 
 @Module({
   imports: [
@@ -17,6 +18,20 @@ import * as Joi from '@hapi/joi';
         DATABASE_DB: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
       }),
+    }),
+    Neo4jModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): Neo4jConfig => {
+        return {
+          scheme: configService.get('DATABASE_SCHEME'),
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get('DATABASE_PORT'),
+          username: configService.get('DATABASE_USER'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_DB'),
+        };
+      },
     }),
     ProblemsModule,
   ],
