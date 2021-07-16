cb6cb0620e0d91dd9a5db9a0653ebbe519f6cef4
whddk4415 <whddk4415@gmail.com>
2021-07-16 13:32:09 +0900

    feat: add init module code to problems.module.ts

--- algopa-problem/src/problems/problems.module.ts
+++ algopa-problem/src/problems/problems.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { ProblemsController } from './problems.controller';
 import { ProblemsService } from './problems.service';
+import { Neo4jService } from 'nest-neo4j';
 
 @Module({
   controllers: [ProblemsController],
   providers: [ProblemsService],
 })
-export class ProblemsModule {}
+export class ProblemsModule implements OnModuleInit {
+  constructor(private readonly neo4jService: Neo4jService) {}
+
+  async onModuleInit() {
+    await this.neo4jService
+      .write(`CREATE CONSTRAINT ON (p:Problem) ASSERT p.id IS UNIQUE`)
+      .catch(() => {});
+  }
+}
