6d66fb29d4b0260d99243e89bc297092aaca0b70
whddk4415 <whddk4415@gmail.com>
2021-07-19 14:49:22 +0900

    feat: create method to get roadmap data in problems.service.ts

--- algopa-problem/src/problems/problems.service.ts
+++ algopa-problem/src/problems/problems.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Neo4jService } from 'nest-neo4j/dist';
+import { mockRoadMap } from './mock/roadmap.mock.type';
+import { mockRoadMapData } from './mock/roadmap.mock.data';
 
 @Injectable()
-export class ProblemsService {}
+export class ProblemsService {
+  constructor(private readonly neo4jService: Neo4jService) {}
+
+  async getRoadMap(userId: number): Promise<mockRoadMap> {
+    return mockRoadMapData;
+  }
+}
