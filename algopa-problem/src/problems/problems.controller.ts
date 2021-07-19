f0c2a891adbbec5288df8a0e762171754b41c50c
whddk4415 <whddk4415@gmail.com>
2021-07-19 14:49:47 +0900

    feat: create method to get roadmap data in problems.controller.ts

--- algopa-problem/src/problems/problems.controller.ts
+++ algopa-problem/src/problems/problems.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { ProblemsService } from './problems.service';
+import { VersionGet } from '../common/decorators/version-get.decorator';
+import { UserId } from '../common/decorators/user-id.decorator';
 
 @Controller('problems')
-export class ProblemsController {}
+export class ProblemsController {
+  constructor(private readonly problemsService: ProblemsService) {}
+
+  @VersionGet({ path: '/roadmap', version: 'v1' })
+  async getRoadMap(@UserId() userId: number) {
+    return await this.problemsService.getRoadMap(userId);
+  }
+}
