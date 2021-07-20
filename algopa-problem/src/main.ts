f761e11c4c1ad57569cfff256d1f989d9733bbf0
whddk4415 <whddk4415@gmail.com>
2021-07-20 21:19:23 +0900

    refactor: apply global pipe to main.ts and change port using configService

--- algopa-problem/src/main.ts
+++ algopa-problem/src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ErrorMessagesInterceptor } from './common/interceptors/error.messages.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
   app.enableCors({ origin: '*' });
   app.setGlobalPrefix('api');
-  await app.listen(3001);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ErrorMessagesInterceptor(configService));
+  await app.listen(configService.get('APP_PORT'));
+  Logger.log(
+    `Server running on ${configService.get('APP_DOMAIN')}:${configService.get(
+      'APP_PORT',
+    )}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
