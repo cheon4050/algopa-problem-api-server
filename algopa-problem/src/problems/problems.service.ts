7c7fb5da21b2f681858b44901ae73d984dc33ca4
whddk4415 <whddk4415@gmail.com>
2021-07-20 21:28:01 +0900

    feat: create method to recommend problems using mock data in problems.service.ts

--- algopa-problem/src/problems/problems.service.ts
+++ algopa-problem/src/problems/problems.service.ts
@@ -2,15 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { Neo4jService } from 'nest-neo4j/dist';
 import { MockRoadMapType } from './mock/roadmap.mock.type';
 import { RoadmapMockService } from './mock/roadmap.mock.service';
+import { RecommendationMockType } from './mock/recommendation.mock.type';
+import { RecommendationMockService } from './mock/recommendation.mock.service';
 
 @Injectable()
 export class ProblemsService {
   constructor(
     private readonly neo4jService: Neo4jService,
     private readonly roadmapMockService: RoadmapMockService,
+    private readonly recommendationMockService: RecommendationMockService,
   ) {}
 
   async getRoadMap(userId: number): Promise<MockRoadMapType> {
     return this.roadmapMockService.createRoadMapMockData();
+  }
+
+  async recommendProblem({
+    userId,
+    limit,
+    type,
+  }): Promise<RecommendationMockType[]> {
+    return this.recommendationMockService.getRandomProblems(limit ? limit : 20);
   }
 }
