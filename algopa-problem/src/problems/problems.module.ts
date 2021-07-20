60e644022f88cf8419ac9640de06f6e2cb51a26d
whddk4415 <whddk4415@gmail.com>
2021-07-20 21:23:22 +0900

    feat: import RecommendationMockService to problems.module.ts

--- algopa-problem/src/problems/problems.module.ts
+++ algopa-problem/src/problems/problems.module.ts
@@ -3,10 +3,11 @@ import { ProblemsController } from './problems.controller';
 import { ProblemsService } from './problems.service';
 import { Neo4jService } from 'nest-neo4j';
 import { RoadmapMockService } from './mock/roadmap.mock.service';
+import { RecommendationMockService } from './mock/recommendation.mock.service';
 
 @Module({
   controllers: [ProblemsController],
-  providers: [ProblemsService, RoadmapMockService],
+  providers: [ProblemsService, RoadmapMockService, RecommendationMockService],
 })
 export class ProblemsModule implements OnModuleInit {
   constructor(private readonly neo4jService: Neo4jService) {}
