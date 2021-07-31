145256fab1b7f093a89b534c9f0bb5397d050d03
whddk4415 <whddk4415@gmail.com>
2021-08-01 03:45:30 +0900

    feat: create user decorator

---
+++ algopa-problem/src/common/decorators/user.decorator.ts
@@ -0,0 +1,16 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { IUserRequest } from 'src/problems/interfaces/request/user-request.interface';
+
+export const User = createParamDecorator(
+  (isEssential: boolean, ctx: ExecutionContext): IUserRequest | null => {
+    const request = ctx.switchToHttp().getRequest();
+    if (request.query.email && request.query.provider) {
+      return {
+        email: request.query?.email,
+        provider: request.query?.provider,
+      };
+    } else {
+      return;
+    }
+  },
+);
