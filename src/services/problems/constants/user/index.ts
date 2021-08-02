f96a57fe780eec6a0bb04ea07dff84ea092cab06
cheon4050 <ehdcjs159@gmail.com>
2021-08-02 15:04:48 +0900

    chore: creat_user, create_solved_relation cypher change

--- src/services/problems/constants/user/index.ts
+++ src/services/problems/constants/user/index.ts
@@ -4,7 +4,7 @@ export const GET_ONE_USER = `
 `;
 
 export const CREATE_USER = `
-    merge (u:User {email: $email, provider: $provider, isInit: false})
+    merge (u:User {email: $email, provider: $provider})
     return u
 `;
 
