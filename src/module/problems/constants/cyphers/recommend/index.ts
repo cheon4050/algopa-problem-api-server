f1938f811dd5eaf65c5073f5e4fb312473622310
cheon4050 <ehdcjs159@gmail.com>
2021-10-09 04:32:40 +0900

    feat: Add recommendation query(problemId)

--- src/module/problems/constants/cyphers/recommend/index.ts
+++ src/module/problems/constants/cyphers/recommend/index.ts
@@ -66,3 +66,42 @@ export const RECOMMEND_DESIREDCOMPANY_PROBLEM = `
     return p, c 
     order by ra limit toInteger($limit)
 `;
+
+export const NEXT_RECOMMEND_SIMILAR_PROBLEM = `
+call{
+    match(p:PROBLEM{id:$id})-[r:main_tag]-(mainc:CATEGORY)
+    match(p)-[r1:sub_tag]-(subc:CATEGORY)
+    with (count(r)+count(r1))as cr, p, mainc, subc
+    match(u:USER{email:$email, provider:$provider}),(p1:PROBLEM)-[r2]-(c3:CATEGORY)
+    where (((p1)-[:sub_tag]-(subc) and (p1)-[:main_tag]-(mainc)) or (p1)-[:main_tag]-(mainc)) and not p.id = p1.id and not (p1)-[:solved]-(u)
+    with p1, count(r2) as cr2, cr, p
+    where cr2 <= cr
+    return p1
+    order by cr2 desc, abs(p.level - p1.level)
+union
+    match(u:USER{email:$email, provider:$provider})
+    match(p:PROBLEM{id:$id})-[r:main_tag]-(mainc:CATEGORY)
+    match(p1:PROBLEM)-[:main_tag]-(mainc)
+    where not (p1)-[:solved]-(u) and not p1.id =p.id
+    return p1
+    order by  abs(p.level - p1.level)
+}
+match(p1)-[:main_tag]-(cate:CATEGORY)
+optional match (p1)-[:sub_tag]-(cate2:CATEGORY)
+return distinct p1,  [cate.name]+collect(cate2.name) limit toInteger($limit)
+`;
+
+export const NEXT_RECOMMEND_NEW_CATEGORY_PROBLEM = `
+call{
+    match(u:USER{email:$email, provider:$provider})
+    match(p:PROBLEM{id:$id})-[:main_tag]-(c:CATEGORY)
+    match(c)-[:next]->(c1:CATEGORY)
+    match(c1)-[:main_tag]-(p1:PROBLEM)
+    where not (u)-[:solved]-(p1)
+    return p1
+    order by abs(p.level-p1.level)
+}
+match(p1)-[:sub_tag]-(c2:CATEGORY)
+match(p1)-[:main_tag]-(c1:CATEGORY)
+return p1, [c1.name]+ collect(c2.name) limit toInteger($limit)
+`;
